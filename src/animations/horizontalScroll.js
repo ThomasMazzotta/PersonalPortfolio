91781044cb0eb21a49bd1e79c8dfb1999cbf110c
Thomas Mazzotta <thomas.mazzotta.dv@gmail.com>
2026-02-19 12:01:03 +0100

    Added horizontal scroll with ScrollTrigger

---
+++ src/animations/horizontalScroll.js
@@ -0,0 +1,16 @@
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+gsap.registerPlugin(ScrollTrigger)
+
+export function horizontalScroll() {
+  const contents = gsap.utils.toArray("#horizontal .content")
+
+  gsap.to(contents, {
+    xPercent: -100 * (contents.length - 1),
+    scrollTrigger: {
+      trigger: "#horizontal",
+      pin: true,
+      scrub: 1,
+    },
+  })
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import "./style.css"
 import { startSunWiggle } from "./animations/heroIntro"
 import { textAnimation } from "./animations/heroIntro"
+import { horizontalScroll } from "./animations/horizontalScroll"
 
 startSunWiggle()
 textAnimation()
+horizontalScroll()
